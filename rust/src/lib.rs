8ebaaa531c9dbb581f7c4e3513d834842d008b18
Michael MacLeod <michaelmmacleod@gmail.com>
2025-11-18 02:00:40 -0800

    Try to add doc

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,21 +1,36 @@
 use godot::prelude::*;
 
+/// Hello, world!
 struct MyExtension;
 
+/// Hello, world!
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
 
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
+/// Hello, world!
 struct Player {
+    /// Hello, world!
     base: Base<Sprite2D>,
 }
 
 use godot::classes::{ISprite2D, Sprite2D};
 
 #[godot_api]
+/// Hello, world!
 impl ISprite2D for Player {
+    /// Hello, world!
     fn init(base: Base<Sprite2D>) -> Self {
         Self { base }
+    }
+}
+
+#[godot_api]
+impl Player {
+    /// Hello, world!
+    #[func]
+    pub fn foo(&self) -> f32 {
+        0.0
     }
 }
