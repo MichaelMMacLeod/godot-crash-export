e866c45a2dc4d3c82cdb6cc22797c5ba6970f482
Michael MacLeod <michaelmmacleod@gmail.com>
2025-11-17 22:10:35 -0800

    commit

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Game development flake";
+  description = "Test development flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -22,20 +22,19 @@
           src = ./rust;
           cargoLock.lockFile = ./rust/Cargo.lock;
         };
-        rustPkg = pkgs.rustPlatform.buildRustPackage pkgConfi;
+        rustPkg = pkgs.rustPlatform.buildRustPackage pkgConfig;
       in
       {
         packages = {
-          x86_64-linux-full3 = pkgs.stdenv.mkDerivation {
+          test = pkgs.stdenv.mkDerivation {
             name = "x86_64-linux-full";
             system = system;
             src = ./godot;
 
-            meta.mainProgram = "Game.x86_64";
+            meta.mainProgram = "TestProject.x86_64";
 
             nativeBuildInputs = with pkgs; [
               autoPatchelfHook
-              breakpointHook
               glibc
             ];
 
@@ -65,13 +64,12 @@
 
             GODOT_VERSION_DIR = "4.5.1.stable";
             GODOT_EXPORT_TEMPLATES_DIR = "${pkgs.godotPackages_4_5.export-templates-bin.out}/share/godot/export_templates/";
-            GAME_SHARED_LIBRARY_OUT = "${linuxRustPkg.out}";
+            GAME_SHARED_LIBRARY_OUT = "${rustPkg.out}";
             GODOT_PRESET = "Linux";
             FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
             FONTCONFIG_PATH = "${pkgs.fontconfig.out}/etc/fonts";
 
             buildPhase = builtins.readFile ./scripts/buildPhase.sh;
-            installPhase = builtins.readFile ./scripts/installPhase.sh;
           };
         };
       }
