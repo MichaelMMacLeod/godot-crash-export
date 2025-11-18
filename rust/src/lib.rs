7ea4e1261c36897e617b29f0d15be8b72abbc88c
Michael MacLeod <michaelmmacleod@gmail.com>
2025-11-17 22:23:41 -0800

    Remove unused fields

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,9 +8,6 @@ unsafe impl ExtensionLibrary for MyExtension {}
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
 struct Player {
-    speed: f64,
-    angular_speed: f64,
-
     base: Base<Sprite2D>,
 }
 
@@ -19,12 +16,6 @@ use godot::classes::{ISprite2D, Sprite2D};
 #[godot_api]
 impl ISprite2D for Player {
     fn init(base: Base<Sprite2D>) -> Self {
-        godot_print!("Hello, world!"); // Prints to the Godot console
-
-        Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
-            base,
-        }
+        Self { base }
     }
 }
