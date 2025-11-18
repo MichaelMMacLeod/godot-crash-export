ad393b15c40cbf9c55260a2f8e98193753a9f21b
Michael MacLeod <michaelmmacleod@gmail.com>
2025-11-17 22:52:54 -0800

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -26,54 +26,52 @@
         godotVersion = builtins.replaceStrings [ "-" ] [ "." ] pkgs.godot.version;
       in
       {
-        packages = {
-          test = pkgs.stdenv.mkDerivation {
-            name = "x86_64-linux-full";
-            system = system;
-            src = ./godot;
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "x86_64-linux-full";
+          system = system;
+          src = ./godot;
 
-            meta.mainProgram = "TestProject.x86_64";
+          meta.mainProgram = "TestProject.x86_64";
 
-            nativeBuildInputs = with pkgs; [
-              autoPatchelfHook
-              glibc
-            ];
+          nativeBuildInputs = with pkgs; [
+            autoPatchelfHook
+            glibc
+          ];
 
-            buildInputs = with pkgs; [
-              makeWrapper
-              rustPkg
-              godot
-            ];
+          buildInputs = with pkgs; [
+            makeWrapper
+            rustPkg
+            godot
+          ];
 
-            runtimeDependencies = with pkgs; [
-              vulkan-loader
-              libGL
-              xorg.libX11
-              xorg.libXcursor
-              xorg.libXinerama
-              xorg.libXext
-              xorg.libXrandr
-              xorg.libXrender
-              xorg.libXi
-              xorg.libXfixes
-              libxkbcommon
-              alsa-lib
-              fontconfig.lib
-              dbus.lib
-              pulseaudio.out
-            ];
+          runtimeDependencies = with pkgs; [
+            vulkan-loader
+            libGL
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXinerama
+            xorg.libXext
+            xorg.libXrandr
+            xorg.libXrender
+            xorg.libXi
+            xorg.libXfixes
+            libxkbcommon
+            alsa-lib
+            fontconfig.lib
+            dbus.lib
+            pulseaudio.out
+          ];
 
-            GODOT_VERSION = "${godotVersion}";
-            GODOT_EXPORT_TEMPLATES_NIX_DIR = "${pkgs.godotPackages.export-templates-bin.out}/share/godot/export_templates";
-            GODOT_EXPORT_TEMPLATES_LOCAL_DIR = ".local/share/godot/export_templates";
+          GODOT_VERSION = "${godotVersion}";
+          GODOT_EXPORT_TEMPLATES_NIX_DIR = "${pkgs.godotPackages.export-templates-bin.out}/share/godot/export_templates";
+          GODOT_EXPORT_TEMPLATES_LOCAL_DIR = ".local/share/godot/export_templates";
 
-            GAME_SHARED_LIBRARY_OUT = "${rustPkg.out}";
-            GODOT_PRESET = "Linux";
-            FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
+          GAME_SHARED_LIBRARY_OUT = "${rustPkg.out}";
+          GODOT_PRESET = "Linux";
+          FONTCONFIG_FILE = "${pkgs.fontconfig.out}/etc/fonts/fonts.conf";
 
-            buildPhase = builtins.readFile ./scripts/buildPhase.sh;
-            installPhase = builtins.readFile ./scripts/installPhase.sh;
-          };
+          buildPhase = builtins.readFile ./scripts/buildPhase.sh;
+          installPhase = builtins.readFile ./scripts/installPhase.sh;
         };
       }
     );
